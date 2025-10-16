48c53545d935274dff998a0d3c9fa4c683f1c76e
rmoo763 <rmoo763@aucklanduni.ac.nz>
2025-10-16 13:51:18 +1300

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -103,33 +103,44 @@ var_durations = pd.DataFrame(durations.to_numpy() * multiplier_matrix, index=dur
 print(var_durations)
 
 routes = [
-    ["Woolworths Porirua", "Woolworths Johnsonville"],
-    ["Woolworths Upper Hutt", "Woolworths Maidstone"],
-    ["Metro Cable Car Lane", "Woolworths Karori", "Woolworths Crofton Downs"],
-    ["Woolworths Kilbirnie", "Woolworths Newtown", "FreshChoice Cuba Street"],
-    ["Woolworths Petone", "Woolworths Wainuiomata", "FreshChoice Woburn"],
-    ["Woolworths Johnsonville Mall", "Woolworths Queensgate", "Woolworths Lower Hutt"],
-    ["Woolworths Aotea", "FreshChoice Cannons Creek", "Woolworths Tawa"]
+    ["CentrePort Wellington","Woolworths Porirua", "Woolworths Johnsonville","CentrePort Wellington"],
+    ["CentrePort Wellington","Woolworths Upper Hutt", "Woolworths Maidstone","CentrePort Wellington"],
+    ["CentrePort Wellington","Metro Cable Car Lane", "Woolworths Karori", "Woolworths Crofton Downs","CentrePort Wellington"],
+    ["CentrePort Wellington","Woolworths Kilbirnie", "Woolworths Newtown", "FreshChoice Cuba Street","CentrePort Wellington"],
+    ["CentrePort Wellington","Woolworths Petone", "Woolworths Wainuiomata", "FreshChoice Woburn","CentrePort Wellington"],
+    ["CentrePort Wellington","Woolworths Johnsonville Mall", "Woolworths Queensgate", "Woolworths Lower Hutt","CentrePort Wellington"],
+    ["CentrePort Wellington","Woolworths Aotea", "FreshChoice Cannons Creek", "Woolworths Tawa","CentrePort Wellington"]
 ]
 
 demands = bootstrap_weekday_demand(weekday_demand_distribution)
 
 route_demand_dict_working = {}
 route_demand_dict_not_working = {}
-
+route_names_working = []
+total_demand_working = []
 for route in routes:
     total_demand = 0
     for store in route:
         total_demand += demands[store]
     # make the route name a simple string (normal dict key)
-    route_name = " ->".join(route)
+    route_name = '->'.join(route)
     if total_demand <= 9:
+        route_names_working.append(route_name)
+        total_demand_working.append(total_demand)
         route_demand_dict_working[route_name] = total_demand
     else:
         route_demand_dict_not_working[route_name] = total_demand
 
 print(route_demand_dict_working)
+
+
+
 print(route_demand_dict_not_working)
+print(route_names_working)
+print(total_demand_working)
+
+
+
 
 
 
